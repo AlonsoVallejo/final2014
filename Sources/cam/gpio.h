09d461923b9f9319407df63c3e5833e2c7adb438
oana.irina.mihai@gmail.com <oana.irina.mihai@gmail.com@f3021f6c-407f-a3cd-e853-a9d1a581fd7a>
2014-01-28 00:53:45 +0000

    PWM testat! - mai are nevoie de pit pt pwm

--- Sources/cam/gpio.h
+++ Sources/cam/gpio.h
@@ -1,31 +1,12 @@
 /*
  * gpio.h
  *
- *  Created on: 14.01.2014
+ *  Created on: 28.01.2014
  *      Author: Iulian
  */
 
 #ifndef GPIO_H_
 #define GPIO_H_
-
-#include "derivative.h"
-
-/*		defines		*/
-
-/*SECTION signal actions*/
-#define SELECTION_MASK		(1 << 26)
-#define SELECTION_OFF		GPIOE_PDOR &= ~(SELECTION_MASK);
-#define SELECTION_ON		GPIOE_PDOR |= SELECTION_MASK;
-
-/*ENDL signal actions*/
-#define ENDL_MASK		(1 << 14)
-#define ENDL_OFF		GPIOD_PDOR &= ~(ENDL_MASK);
-#define ENDL_ON			GPIOD_PDOR |= ENDL_MASK;
-
-/*ENDR signal actions*/
-#define ENDR_MASK		(1 << 3)
-#define ENDR_OFF		GPIOB_PDOR &= ~(ENDR_MASK);
-#define ENDR_ON			GPIOB_PDOR |= ENDR_MASK;
 
 
 #endif /* GPIO_H_ */
